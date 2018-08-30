af9819b6fc1905091f78d2edf1c822b0936e6f18
Andrew Nguyen <andrew.ntranced@gmail.com>
2018-08-30 20:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var name1 = "Joe"
+var name = "Joe"
 var height = "74"
- 
-if (name1 === "Susan") {
-  console.log("The name is not valid")
-}
  
 // Don't worry about this
 module.exports = { name, height
